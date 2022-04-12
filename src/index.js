581a3a885c549883e5041a0936ac8eeb78625853
Remon <remon_028@outlook.com>
2022-04-12 14:22:00 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+// import Cat from './images/cat.jpg';
+
+// function component() {
+//     const element = document.createElement('div');
+  
+    // const myIcon = new Image();
+    // myIcon.src = Cat;
+
+//     element.appendChild(myIcon);
+
+
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
+
